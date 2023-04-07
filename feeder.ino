7ff78c7b75c03c552fa1101c2ab33f3652614eca
Peter Peng <yser.pc@gmail.com>
2023-04-07 17:11:36 -0400

    set time on web page

--- feeder.ino
+++ feeder.ino
@@ -83,7 +83,6 @@ void setup()
   delay(500);
 }
 
-// Task1code: blinks an LED every 1000 ms
 void Task1code(void *pvParameters)
 {
   Serial.print("Task1 running on core ");
@@ -122,7 +121,13 @@ void Task1code(void *pvParameters)
                 {
                   feed();
                 }
+              } else if (header.indexOf("GET /?time=") >= 0)
+              {
+                pos1 = header.indexOf('=');
+                pos2 = header.indexOf('&');
+                setTime = header.substring(pos1 + 1, pos2).toInt();
               }
+              Serial.println("Time set: " + String(setTime));
               client.println(SendHTML());
               client.println();
               break;
@@ -145,7 +150,6 @@ void Task1code(void *pvParameters)
   }
 }
 
-// Task2code: blinks an LED every 700 ms
 void Task2code(void *pvParameters)
 {
   Serial.print("Task2 running on core ");
@@ -155,10 +159,7 @@ void Task2code(void *pvParameters)
   {
     timeClient.update();
     timeStamp = timeClient.getEpochTime();
-    Serial.println(String(timeStamp));
-
     int mod = (timeStamp - setTime) % 86400;
-    Serial.println(String(mod));
     if (mod == 0) {
       Serial.println(timeClient.getFormattedTime());
       feed();
@@ -173,27 +174,61 @@ void loop()
 
 String SendHTML()
 {
+  String hour;
+  String minute;
+  int hr = setTime / 3600;
+  if (hr < 10) {
+    hour = "0" + String(hr);
+  } else {
+    hour = String(hr);
+  }
+  
+  int min = (setTime - hr * 3600) / 60;
+  if (min < 10) {
+    minute = "0" + String(min);
+  } else {
+    minute = String(min);
+  }
+  Serial.println(hour + ":" + minute);
+
   String ptr = "<!DOCTYPE html> <html>\n";
   ptr += "<head>\n";
   ptr += "<title>Fish Feeder</title>\n";
   ptr += "<style>\n";
   ptr += "body{margin-top: 5rem;text-align: center;}\n";
-  ptr += "#feed {display:block;background-color: #3498db;border:none;color:white;padding:1rem;font-size:2rem;margin-left:auto;margin-right:auto;border-radius:5%;}\n";
+  ptr += "#feed {display:block;background-color: #3498db;border:none;color:white;padding:1rem;font-size:2rem;margin: 1rem auto 1rem auto;border-radius:5%;}\n";
   ptr += "p {color:red;}\n";
+  ptr += "";
+  ptr += "label { display: block; font: 1rem 'Fira Sans', sans-serif; }";
+  ptr += "input, label { margin: 0.4rem 0; }";
+
   ptr += "</style>\n";
   ptr += "</head>\n";
+
   ptr += "<body>\n";
   ptr += "<h1>Fish Feeder</h1>\n";
   ptr += "<p id=\"helper-text\"></p>";
   ptr += "<button id=\"feed\" onclick=\"feed()\">Feed</button>\n";
+  ptr += "<label for=\"time\">Choose a time for automatic feeding:</label>";
+  ptr += "<input type=\"time\" id=\"time\" name=\"time\" min=\"00:00\" max=\"24:00\" onchange=\"changeTime()\" ";
+  ptr += "value='" + hour + ":" + minute + "' required>\n";
 
   ptr += "<script>\n";
-  ptr += "var button = document.getElementById(\"feed\");\n";
-  ptr += "var helper = document.getElementById(\"helper-text\");\n";
-  ptr += "async function feed() {\n";
+  ptr += "const button = document.getElementById(\"feed\");\n";
+  ptr += "const helper = document.getElementById(\"helper-text\");\n";
+  ptr += "const time = document.getElementById(\"time\");\n";
+
+  ptr += "const feed = async () => {\n";
   ptr += "try { helper.innerHTML = ''; button.innerHTML = \"feeding\"; button.style.backgroundColor = \"grey\"; button.disabled = true; const res = await fetch(\"/?value=1&\");} \n";
   ptr += "catch (err) { console.error(\"Operation failed: \", err); helper.innerHTML = `Operation failed: ${err}`; }";
-  ptr += "finally { setTimeout(() => { button.innerHTML = \"feed\"; button.disabled = false; button.style.backgroundColor = \"#3498db\"; }, \"1000\"); }}";
+  ptr += "finally { setTimeout(() => { button.innerHTML = \"feed\"; button.disabled = false; button.style.backgroundColor = \"#3498db\"; }, \"1000\"); }}\n";
+  
+  ptr += "const changeTime = async () => {\n";
+  ptr += "const val = time.value.split(':'); const hour = parseInt(val[0]); const min = parseInt(val[1]); const newTime = hour * 3600 + min * 60;\n";
+  ptr += "try { helper.innerHTML = ''; time.disabled = true; const res = await fetch(`/?time=${newTime}&`); }\n";
+  ptr += "catch (err) { console.error(\"Failed to set time: \", err); helper.innerHTML = `Failed to set time: ${err}`; }";
+  ptr += "finally { time.disabled = false; } }";
+  
   ptr += "</script>\n";
   ptr += "</body>\n";
   ptr += "</html>\n";
